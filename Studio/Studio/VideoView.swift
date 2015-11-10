236e78b1fbe6caf5f4b07d52f6e1d54c7f00e952
Ludovic Vannoorenberghe <ludovic.vannoorenberghe@keemotion.com>
2015-11-10 09:22:11 +0100

    draft mjpeg view

--- Studio/Studio/VideoView.swift
+++ Studio/Studio/VideoView.swift
@@ -2,8 +2,48 @@
 import Foundation
 import UIKit
 
-class VideoView: UIView {
+class VideoView: UIButton, NSURLConnectionDataDelegate {
+    
+    let url = NSURL(fileURLWithPath: "http://localhost:1337/mjpeg_stream")
+    
+    private var data = NSMutableData()
+
+    weak var connection : NSURLConnection?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        //let image: UIImage = UIImage(imageLiteral: "dd")
+        
+        let request = NSURLRequest(URL: url)
+        
+        connection = NSURLConnection(request: request, delegate: self)
+        
+        if let c = connection {
+            c.start()
+            print("lll")
+        }
+    }
     
     
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        // put through data
+        if let mjpegImage = UIImage(data: data) {
+            imageView?.image = mjpegImage
+            print("hello")
+        }
+        print("hello2")
+        // reset data
+        self.data.length = 0
+    }
     
+    
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        self.data.appendData(data)
+        print("hello3")
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        print(error.localizedDescription)
+    }
 }
